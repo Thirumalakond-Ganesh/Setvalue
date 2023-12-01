f595a0051d864df1bf8e2cca6d3c6a2f35306dd4
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:46:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import { useForm, Controller} from "react-hook-form";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=>{
+  const{control,handleSubmit,setValue}=useForm();
 
+const onSubmit=(data)=>{
+  console.log(data);
+};
+const handleSetValue=()=>{
+  setValue('example','Hello, Welcome to React Hook Form...!');
+};
+
+return(
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      <h2>SetValue Example</h2>
+      <Controller
+      name="example"
+      control={control}
+      defaultValue=""  // we can add default values here
+      render={({field})=>(
+        <input {...field} type="text" placeholder="Type Something..."/>
+      )}
+      />
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+      <button type="button" onClick={handleSetValue}>SetValue</button>
+    </div>
+  </form>
+);
+
+};
 export default App;
